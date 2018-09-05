0f9c5e0fe04511a027a9c641934e340bbae5bff5
Danil <kostrov@gmail.com>
2018-09-06 00:57:43 +0700

    update

--- src/components/app/serverless.js
+++ src/components/app/serverless.js
@@ -16,15 +16,13 @@ const icons = [
 ];
 
 const shuffleArray = (array: string []) => {
-	const result = [];
-
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor (Math.random () * (i + 1));
 
         [array [i], array [j]] = [array [j], array [i]];
     }
 
-    return result;
+    return array;
 }
 
 export default () => shuffleArray (icons);
